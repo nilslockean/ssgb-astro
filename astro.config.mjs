569c9216e1e753f30acc97da94948b48f2f695a7
Nils Lockean <nils@lockean.se>
2026-04-20 08:09:39 +0200

    Remove references to Cooper Hewitt in favour of Barlow Condensed

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,7 +54,7 @@ export default defineConfig({
       name: 'Barlow Condensed',
       cssVariable: '--font-barlow-condensed',
       provider: fontProviders.fontsource(),
-      weights: [400]
+      weights: [400, 500]
     },
     {
       name: "Host Grotesk",
