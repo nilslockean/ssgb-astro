731142ea1b38b8880ab20fcb75102a32e52d88f3
Nils Lockean <nils@lockean.se>
2026-06-23 10:01:48 +0200

    Remove slug from link component test

--- src/components/Link.test.ts
+++ src/components/Link.test.ts
@@ -1,7 +1,6 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { describe, expect, test } from "vitest";
 import Link from "./Link.astro";
-import { getPath, Slug } from "@lib/routeUtils";
 
 describe("Link component", async () => {
   test("renders default props", async () => {
@@ -18,19 +17,5 @@ describe("Link component", async () => {
     // expect(result).toContain("This is a card");
     expect(result).toContain("Link content");
     expect(result).toContain('href="test"');
-  });
-
-  test("prefers slug over href", async () => {
-    const container = await AstroContainer.create();
-    const result = await container.renderToString(Link, {
-      props: {
-        href: "test",
-        slug: Slug.ABOUT,
-      },
-    });
-    const basePath = getPath(Slug.ABOUT);
-
-    expect(result).not.toContain('href="test"');
-    expect(result).toMatch(new RegExp(`href="${basePath}\\/?"`));
   });
 });
